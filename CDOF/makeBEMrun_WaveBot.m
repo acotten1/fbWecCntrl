c457bb21140fcab145280ee99680e1700a3d47b4
acotten <alfred.cotten@mocean.energy>
2021-02-26 12:44:25 +0000

    Added code to compute natural frequencies using different formulae.

--- CDOF/makeBEMrun_WaveBot.m
+++ CDOF/makeBEMrun_WaveBot.m
@@ -62,7 +62,8 @@ box on;
 set(gca, 'view', [10 20]);
 
 % Case 2) - WaveBot with a Compressible Degree Of Freedom (CDOF) defined over its base
-fdComp_CDOF = waveBotComp(rho, h, T, r1, r2, t1, t2, t3, 'motions', [0 0 1 0 0 0 1], 'cdofVol',vol_total*25);
+cdofVol = 25*vol_total;
+fdComp_CDOF = waveBotComp(rho, h, T, r1, r2, t1, t2, t3, 'motions', [0 0 1 0 0 0 1], 'cdofVol',cdofVol);
 
 % Plot heave motions
 motions_Rigid = fdComp_Rigid.Motions;
@@ -91,4 +92,73 @@ xlabel('Frequency (Hz)');
 ylabel('Power RAO (kW/m^2)')
 
 save('C:\Users\AlfredCotten\Desktop\MATLAB\snl_fbWecCntrlFork_CDOF\CDOF\data\waveBot_r0.88_CDOF.mat','fdComp_Rigid','fdComp_CDOF')
+
+%% Compute lower limit on natural frequency (V0 --> Inf)
+f = 1./T';
+pAtm = 101325; % Atmospheric pressure (in Pascals)
+g = 9.80665; % gravitational acceleration
+p0 = (pAtm + rho*g*t2);
+adInd = 1.4; % Adiabatic index for air
+S1 = pi*r1^2; S2 = pi*r2^2;
+
+a = (fdComp_CDOF.M(1,1)+fdComp_CDOF.A(:,1,1)).*fdComp_CDOF.A(:,2,2) - fdComp_CDOF.A(:,1,2).^2;
+b = -((fdComp_CDOF.M(1,1)+fdComp_CDOF.A(:,1,1)).*(rho*g*S1) + rho*g*S2*fdComp_CDOF.A(:,2,2) + fdComp_CDOF.B(:,1,1).*fdComp_CDOF.B(:,2,2) ...
+    - 2*rho*g*S1*fdComp_CDOF.A(:,1,2) - fdComp_CDOF.B(:,1,2).^2);
+c = rho*g*S1*(rho*g*S2 - rho*g*S1);
+wresSquared = (- b - sqrt(b.^2 - 4*a*c))./(2*a); % Solve for omega squared (smallest of the two solutions is of interest here)
+wres = sqrt(wresSquared); % Only positive solution is of interest here
+fres = wres/(2*pi);
+[val ind] = min(abs(fres - f)); % 
+fres(ind)
+
+%% Compute lower limit on natural frequency using simpler formula
+wres = sqrt(rho*g*(S2-S1)./(fdComp_CDOF.M(1,1)+fdComp_CDOF.A(:,1,1)-fdComp_CDOF.A(:,1,2)));
+fres = wres/(2*pi);
+[val ind] = min(abs(fres - f)); % 
+fres(ind)
+
+%% Compute natural frequency (no limits taken)
+a = (fdComp_CDOF.M(1,1)+fdComp_CDOF.A(:,1,1)).*fdComp_CDOF.A(:,2,2) - fdComp_CDOF.A(:,1,2).^2;
+b = -((fdComp_CDOF.M(1,1)+fdComp_CDOF.A(:,1,1)).*(rho*g*S1 + S1^2*adInd*p0/cdofVol) + rho*g*S2*fdComp_CDOF.A(:,2,2) + fdComp_CDOF.B(:,1,1).*fdComp_CDOF.B(:,2,2) ...
+    - 2*rho*g*S1*fdComp_CDOF.A(:,1,2) - fdComp_CDOF.B(:,1,2).^2);
+c = rho*g*S1*(rho*g*S2 - rho*g*S1) + rho*g*S2*S1^2*adInd*p0/cdofVol;
+wresSquared = (- b - sqrt(b.^2 - 4*a*c))./(2*a); % Solve for omega squared (smallest of the two solutions is of interest here)
+wres = sqrt(wresSquared); % Only positive solution is of interest here
+fres = wres/(2*pi);
+[val ind] = min(abs(fres - f)); % 
+fres(ind)
+
+% % Make plots to sketch the dependence on each variable
+% % S1
+% S1range = 0:S2/100:S2; index = 50;
+% a = (fdComp_CDOF.M(1,1)+fdComp_CDOF.A(index,1,1)).*fdComp_CDOF.A(index,2,2) - fdComp_CDOF.A(index,1,2).^2;
+% b = -((fdComp_CDOF.M(1,1)+fdComp_CDOF.A(index,1,1)).*(rho*g*S1range + S1range.^2*adInd*p0/cdofVol) + rho*g*S2*fdComp_CDOF.A(index,2,2) + fdComp_CDOF.B(index,1,1).*fdComp_CDOF.B(index,2,2) ...
+%     - 2*rho*g*S1range*fdComp_CDOF.A(index,1,2) - fdComp_CDOF.B(index,1,2).^2);
+% c = rho*g*S1range.*(rho*g*S2 - rho*g*S1range) + rho*g*S2*S1range.^2*adInd*p0/cdofVol;
+% wresSquared = (- b - sqrt(b.^2 - 4*a*c))./(2*a); % Solve for omega squared (smallest of the two solutions is of interest here)
+% wres = sqrt(wresSquared); % Only positive solution is of interest here
+% fres = wres/(2*pi);
+% figure;plot(S1range/S2,fres);xlabel('S_1/S_2');ylabel('Natural frequency (Hz)')
+
+
+%% Compute natural frequency using simpler formula
+r = -rho*g/(S1^2*adInd*p0/cdofVol + rho*g);
+wres = sqrt(rho*g*(S2+S1*r)./(fdComp_CDOF.M(1,1)+fdComp_CDOF.A(:,1,1)+fdComp_CDOF.A(:,1,2)*r));
+fres = wres/(2*pi);
+[val ind] = min(abs(fres - f)); % 
+fres(ind)
+
+% % Make plots to sketch the dependence on each variable
+% % S1
+% S1range = 0:S2/100:S2; index = 50;
+% rrange = -rho*g/(S1^2*adInd*p0/cdofVol + rho*g);
+% wres = sqrt(rho*g*(S2+S1range.*rrange)./(fdComp_CDOF.M(1,1)+fdComp_CDOF.A(index,1,1)+fdComp_CDOF.A(index,1,2)*rrange));
+% fres = wres/(2*pi);
+% figure;plot(S1range/S2,fres,'--k');xlabel('S_1/S_2');ylabel('Natural frequency (Hz)')
+
+%% Rigid body resonant frequency
+wresRB = sqrt(rho*g*S2./(fdComp_CDOF.M(1,1)+fdComp_CDOF.A(:,1,1)));
+fresRB = wresRB/(2*pi);
+[val ind] = min(abs(fresRB - f)); % 
+fresRB(ind)
 
